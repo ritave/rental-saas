3351f6a4957148622c6fd05971e5ee01d83b3e10
cymerrad <cymerrad+git@gmail.com>
2018-02-13 14:34:36 +0100

    YO DAWG I HERD YOU LIKE VARIABLES SO WE PUT VARIABLES IN YO VARIABLES SO YOU CAN COMPUTE WHILE YOU COMPUTE

--- tests/findChanged_test.go
+++ tests/findChanged_test.go
@@ -32,31 +32,33 @@ func TestCompareSortable(t *testing.T) {
 		tBS[i] = tts(timeBack[i])
 	}
 
-	var exhibit1 = &objects.Event{"summary", "user1@mail.com", tts(zeroth), tts(first), "location1", tBS[0]}
-	var exhibit1ModifiedTimeForward = &objects.Event{"summary", "user1@mail.com", tts(first), tts(second), "location1", tBS[0]}
-	var exhibit1ModifiedTimeForwardAndPlace = &objects.Event{"summary", "user1@mail.com", tts(first), tts(second), "location1-modified", tBS[0]}
+	// YO DAWG I HERD YOU LIKE VARIABLES SO WE PUT VARIABLES IN YO VARIABLES SO YOU CAN COMPUTE WHILE YOU COMPUTE
 
-	var exhibit2 = &objects.Event{"summary", "user2@mail.com", tts(first), tts(second), "location2", tBS[1]}
-	var exhibit2ModifiedTimeBackward = &objects.Event{"summary", "user2@mail.com", tts(zeroth), tts(first), "location2", tBS[1]}
-	var exhibit2ModifiedTimeBackwardAndPlace = &objects.Event{"summary", "user2@mail.com", tts(zeroth), tts(first), "location2-modified", tBS[1]}
+	var xzibit1 = &objects.Event{"summary", "user1@mail.com", tts(zeroth), tts(first), "location1", tBS[0]}
+	var xzibit1ModifiedTimeForward = &objects.Event{"summary", "user1@mail.com", tts(first), tts(second), "location1", tBS[0]}
+	var xzibit1ModifiedTimeForwardAndPlace = &objects.Event{"summary", "user1@mail.com", tts(first), tts(second), "location1-modified", tBS[0]}
 
-	var exhibit3 = &objects.Event{"summary", "user3@mail.com", tts(second), tts(third), "location3", tBS[2]}
-	var exhibit3ModifiedPlace = &objects.Event{"summary", "user3@mail.com", tts(second), tts(third), "location3-modified", tBS[2]}
+	var xzibit2 = &objects.Event{"summary", "user2@mail.com", tts(first), tts(second), "location2", tBS[1]}
+	var xzibit2ModifiedTimeBackward = &objects.Event{"summary", "user2@mail.com", tts(zeroth), tts(first), "location2", tBS[1]}
+	var xzibit2ModifiedTimeBackwardAndPlace = &objects.Event{"summary", "user2@mail.com", tts(zeroth), tts(first), "location2-modified", tBS[1]}
 
-	var exhibit4 = &objects.Event{"summary", "user4@mail.com", tts(third), tts(fourth), "location4", tBS[3]}
-	var exhibit4SecondEvent = &objects.Event{"summary", "user4@mail.com", tts(fifth), tts(sixth), "location4-some-other", tBS[6]}
-	var exhibit4ThirdEvent = &objects.Event{"summary", "user4@mail.com", tts(fifth), tts(sixth), "location4", tBS[7]}
+	var xzibit3 = &objects.Event{"summary", "user3@mail.com", tts(second), tts(third), "location3", tBS[2]}
+	var xzibit3ModifiedPlace = &objects.Event{"summary", "user3@mail.com", tts(second), tts(third), "location3-modified", tBS[2]}
 
-	var exhibit5 = &objects.Event{"summary", "user5@mail.com", tts(fourth), tts(fifth), "location5", tBS[4]}
+	var xzibit4 = &objects.Event{"summary", "user4@mail.com", tts(third), tts(fourth), "location4", tBS[3]}
+	var xzibit4SecondEvent = &objects.Event{"summary", "user4@mail.com", tts(fifth), tts(sixth), "location4-some-other", tBS[6]}
+	var xzibit4ThirdEvent = &objects.Event{"summary", "user4@mail.com", tts(fifth), tts(sixth), "location4", tBS[7]}
 
-	var exhibit6 = &objects.Event{"summary", "user6@mail.com", tts(fifth), tts(sixth), "location6", tBS[5]}
+	var xzibit5 = &objects.Event{"summary", "user5@mail.com", tts(fourth), tts(fifth), "location5", tBS[4]}
 
-	//log.Printf("exhibit 1 %p %v\n", exhibit1, *exhibit1)
-	//log.Printf("exhibit 2 %p %v\n", exhibit2, *exhibit2)
-	//log.Printf("exhibit 3 %p %v\n", exhibit3, *exhibit3)
-	//log.Printf("exhibit 4_1 %p %v\n", exhibit4, *exhibit4)
-	//log.Printf("exhibit 4_2 %p %v\n", exhibit4SecondEvent, *exhibit4SecondEvent)
-	//log.Printf("exhibit 4_3 %p %v\n", exhibit4ThirdEvent, *exhibit4ThirdEvent)
+	var xzibit6 = &objects.Event{"summary", "user6@mail.com", tts(fifth), tts(sixth), "location6", tBS[5]}
+
+	//log.Printf("xzibit 1 %p %v\n", xzibit1, *xzibit1)
+	//log.Printf("xzibit 2 %p %v\n", xzibit2, *xzibit2)
+	//log.Printf("xzibit 3 %p %v\n", xzibit3, *xzibit3)
+	//log.Printf("xzibit 4_1 %p %v\n", xzibit4, *xzibit4)
+	//log.Printf("xzibit 4_2 %p %v\n", xzibit4SecondEvent, *xzibit4SecondEvent)
+	//log.Printf("xzibit 4_3 %p %v\n", xzibit4ThirdEvent, *xzibit4ThirdEvent)
 
 
 	type args struct {
@@ -72,68 +74,68 @@ func TestCompareSortable(t *testing.T) {
 		{"empty", args{objects.SortableEvents{}, objects.SortableEvents{}}, []*objects.EventModified{}, false},
 
 		{"nothing changed", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
 		}, []*objects.EventModified{}, false},
 
 		{"2 added inside", args{
-			objects.SortableEvents{exhibit1, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-		}, []*objects.EventModified{objects.NewModified(exhibit2).Flag(objects.Added)}, false},
+			objects.SortableEvents{xzibit1, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+		}, []*objects.EventModified{objects.NewModified(xzibit2).Flag(objects.Added)}, false},
 
 		{"2 deleted inside", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit3, exhibit4, exhibit5, exhibit6},
-		}, []*objects.EventModified{objects.NewModified(exhibit2).Flag(objects.Deleted)}, false},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit3, xzibit4, xzibit5, xzibit6},
+		}, []*objects.EventModified{objects.NewModified(xzibit2).Flag(objects.Deleted)}, false},
 
 		{"1 modified time forwards", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1ModifiedTimeForward, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-		}, []*objects.EventModified{objects.NewModified(exhibit1ModifiedTimeForward).Flag(objects.ModifiedTime)}, false},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1ModifiedTimeForward, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+		}, []*objects.EventModified{objects.NewModified(xzibit1ModifiedTimeForward).Flag(objects.ModifiedTime)}, false},
 
 		{"2 modified time backwards", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit2ModifiedTimeBackward, exhibit3, exhibit4, exhibit5, exhibit6},
-		}, []*objects.EventModified{objects.NewModified(exhibit2ModifiedTimeBackward).Flag(objects.ModifiedTime)}, false},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit2ModifiedTimeBackward, xzibit3, xzibit4, xzibit5, xzibit6},
+		}, []*objects.EventModified{objects.NewModified(xzibit2ModifiedTimeBackward).Flag(objects.ModifiedTime)}, false},
 
 		{"1 modified time forwards and place", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1ModifiedTimeForwardAndPlace, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-		}, []*objects.EventModified{objects.NewModified(exhibit1ModifiedTimeForwardAndPlace).Flag(objects.ModifiedTime).Flag(objects.ModifiedLocation)}, false},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1ModifiedTimeForwardAndPlace, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+		}, []*objects.EventModified{objects.NewModified(xzibit1ModifiedTimeForwardAndPlace).Flag(objects.ModifiedTime).Flag(objects.ModifiedLocation)}, false},
 
 		{"2 modified time backwards and place", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit2ModifiedTimeBackwardAndPlace, exhibit3, exhibit4, exhibit5, exhibit6},
-		}, []*objects.EventModified{objects.NewModified(exhibit2ModifiedTimeBackwardAndPlace).Flag(objects.ModifiedTime).Flag(objects.ModifiedLocation)}, false},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit2ModifiedTimeBackwardAndPlace, xzibit3, xzibit4, xzibit5, xzibit6},
+		}, []*objects.EventModified{objects.NewModified(xzibit2ModifiedTimeBackwardAndPlace).Flag(objects.ModifiedTime).Flag(objects.ModifiedLocation)}, false},
 
 		{"3 modified place", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3ModifiedPlace, exhibit4, exhibit5, exhibit6},
-		}, []*objects.EventModified{objects.NewModified(exhibit3ModifiedPlace).Flag(objects.ModifiedLocation)}, false},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3ModifiedPlace, xzibit4, xzibit5, xzibit6},
+		}, []*objects.EventModified{objects.NewModified(xzibit3ModifiedPlace).Flag(objects.ModifiedLocation)}, false},
 
 		{"5 added, 3 deleted and 2 modified", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit2ModifiedTimeBackward, exhibit4, exhibit5, exhibit6},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit2ModifiedTimeBackward, xzibit4, xzibit5, xzibit6},
 		}, []*objects.EventModified{
-			objects.NewModified(exhibit2ModifiedTimeBackward).Flag(objects.ModifiedTime),
-			objects.NewModified(exhibit3).Flag(objects.Deleted),
-			objects.NewModified(exhibit5).Flag(objects.Added),
+			objects.NewModified(xzibit2ModifiedTimeBackward).Flag(objects.ModifiedTime),
+			objects.NewModified(xzibit3).Flag(objects.Deleted),
+			objects.NewModified(xzibit5).Flag(objects.Added),
 		}, false},
 
 		{"1 and 2 swapped places", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit2ModifiedTimeBackward, exhibit1ModifiedTimeForward, exhibit3, exhibit4, exhibit5, exhibit6},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit2ModifiedTimeBackward, xzibit1ModifiedTimeForward, xzibit3, xzibit4, xzibit5, xzibit6},
 		}, []*objects.EventModified{
-			objects.NewModified(exhibit1ModifiedTimeForward).Flag(objects.ModifiedTime),
-			objects.NewModified(exhibit2ModifiedTimeBackward).Flag(objects.ModifiedTime),
+			objects.NewModified(xzibit1ModifiedTimeForward).Flag(objects.ModifiedTime),
+			objects.NewModified(xzibit2ModifiedTimeBackward).Flag(objects.ModifiedTime),
 		}, false},
 
 		{"4 added two more events", args{
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6},
-			objects.SortableEvents{exhibit1, exhibit2, exhibit3, exhibit4, exhibit5, exhibit6, exhibit4SecondEvent, exhibit4ThirdEvent},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6},
+			objects.SortableEvents{xzibit1, xzibit2, xzibit3, xzibit4, xzibit5, xzibit6, xzibit4SecondEvent, xzibit4ThirdEvent},
 		}, []*objects.EventModified{
-			objects.NewModified(exhibit4SecondEvent).Flag(objects.Added),
-			objects.NewModified(exhibit4ThirdEvent).Flag(objects.Added),
+			objects.NewModified(xzibit4SecondEvent).Flag(objects.Added),
+			objects.NewModified(xzibit4ThirdEvent).Flag(objects.Added),
 		}, false},
 	}
 
