614b9fcc923290263fd5ce520999cc4813e47202
cymerrad <cymerrad+git@gmail.com>
2018-02-20 17:26:22 +0100

    It would be best to just start the project anew - this notifications are irritating | Also file endings...

--- app_engine/notify/notify.go
+++ app_engine/notify/notify.go
@@ -7,9 +7,13 @@ import (
 	"context"
 	"calendar-synch/logic"
 	"google.golang.org/api/calendar/v3"
+	"os"
 )
 
 const NotifyGet = "/notify/get"
+
+const EnvAppPing = "APP_PING"
+const EnvAppChanged = "APP_CHANGED"
 
 func main() {
 	http.HandleFunc(NotifyGet, HandlerGet)
@@ -18,7 +22,9 @@ func main() {
 }
 
 func HandlerGet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Ok, I got this."))
+	w.Write([]byte("Thanks Google, I got this from here."))
+
+	notifyMainApp()
 }
 
 func init() {
@@ -31,7 +37,13 @@ func init() {
 }
 
 func testPing() {
-	resp, err := http.DefaultClient.Get("https://calendar-cron.appspot.com/event/ping")
+	pingAddr, exists := os.LookupEnv(EnvAppPing)
+	if !exists {
+		pingAddr = "https://calendar-cron.appspot.com/event/ping"
+		log.Printf("Resolving to prior default: %s", pingAddr)
+	}
+
+	resp, err := http.DefaultClient.Get(pingAddr)
 	if err != nil {
 		log.Printf("Request on init: %s", err.Error())
 	} else {
@@ -46,5 +58,30 @@ func registerReceiver(cal *calendar.Service) {
 	// TODO error handling
 
 	// TODO refresh after every some constant time interval?
-	logic.WatchForChanges(cal, "https://calendar-cron.appspot.com" + NotifyGet)
+	log.Println("Registering receiver")
+	selfAddr, exists := os.LookupEnv(EnvAppChanged)
+	if !exists {
+		selfAddr = "https://calendar-cron.appspot.com/"
+		log.Printf("Registering: resolving to prior default: %s", selfAddr)
+	}
+	err := logic.WatchForChanges(cal, selfAddr + NotifyGet)
+	if err != nil {
+		log.Printf("Error sending watch request: %s", err.Error())
+	}
+}
+
+func notifyMainApp() {
+	pingAddr, exists := os.LookupEnv(EnvAppChanged)
+	if !exists {
+		pingAddr = "https://calendar-cron.appspot.com/event/changed"
+		log.Printf("Notify: resolving to prior default: %s", pingAddr)
+	}
+
+	resp, err := http.DefaultClient.Get(pingAddr)
+	if err != nil {
+		log.Printf("Notifying: %s", err.Error())
+	} else {
+		log.Printf("Notifying: %d", resp.StatusCode)
+	}
+
 }
--- logic/watchForChanges.go
+++ logic/watchForChanges.go
@@ -3,22 +3,43 @@ package logic
 import (
 	"google.golang.org/api/calendar/v3"
 	"log"
-	"math"
 	"github.com/satori/go.uuid"
+	"time"
 )
 
-func WatchForChanges(cal *calendar.Service, receiver string) {
+func WatchForChanges(cal *calendar.Service, receiver string) (error) {
 	u := uuid.Must(uuid.NewV4())
 
 	channel := calendar.Channel{
 		Id: u.String(),
 		Address: receiver,
 		Type: "web_hook",
-		Expiration: math.MaxInt64, // lol
+		Expiration: time.Now().Add(time.Hour).UnixNano(),
 	}
-	resp, err := cal.Events.Watch("primary", &channel).Do()
+	watchChannel, err := cal.Events.Watch("primary", &channel).Do()
+
+	// TODO do something with this channel!
 	if err != nil {
-		log.Fatalf("Error sending watch request: %s", err.Error())
+		log.Println("Watch response: \n", watchChannel)
+		watchChannel.Expiration = time.Now().Add(time.Minute).UnixNano()
+		resp, err := cal.Events.Watch("primary", watchChannel).Do()
+		if err != nil {
+			log.Println("Editing the channel failed")
+		}
+		log.Println("Second watch response: \n", resp)
 	}
-	log.Println("Watch response: \n", resp)
+
+	// testing cancelling saved watch requests
+	testChannel := calendar.Channel{
+		Id: "1f672443-ef5b-4d43-a232-b36e86bb7b79",
+		Type: "web_hook",
+		Address: receiver,
+		ResourceId: "1521728085000",
+	}
+	err = cal.Channels.Stop(&testChannel).Do()
+	if err != nil {
+		log.Println("Deleting the channel failed")
+	}
+
+	return err
 }
