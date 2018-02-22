fd6f441f19a8e4c531f5874a35537102fa176b08
cymerrad <cymerrad+git@gmail.com>
2018-02-22 17:23:25 +0100

    Finally somewhat working example

--- front/my_try2.elm
+++ front/my_try2.elm
@@ -3,6 +3,11 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import Http
 
+import Json.Encode as Encode
+import Json.Decode as Decode
+
+import Debug
+
 main =
   Html.program
     { init = init
@@ -11,9 +16,10 @@ main =
     , subscriptions = subscriptions
     }
 
-eventServer : String
-eventServer =
-    ""
+apiEventCreate : String
+apiEventCreate =
+--    "https://calendar-cron.appspot.com/event/create"
+    "http://localhost:8080/event/create"
 
 -- MODEL
 
@@ -23,17 +29,35 @@ type alias Model =
     , location : String
 --    , start : Date
 --    , end : Date
+    , error : String
     }
 
 model : Model
-model =
-  Model "" "" ""
+model = startUpValue
+
+
+formEncoder : Model -> Encode.Value
+formEncoder model =
+    Encode.object
+        [ ("summary", Encode.string model.summary)
+        , ("user", Encode.string model.user)
+        , ("location", Encode.string model.location)
+        , ("start", Encode.string "wat")
+        , ("end", Encode.string "wat")
+        ]
+
+postEventResponseDecoder : Decode.Decoder String
+postEventResponseDecoder =
+    Decode.string
 
 -- INIT
 
+startUpValue : Model
+startUpValue = Model "Summary" "radekantichrist@gmail.com" "Location" ""
+
 init : (Model, Cmd Msg)
 init =
-    (model, Cmd.none)
+    (startUpValue, Cmd.none)
 
 -- UPDATE
 
@@ -43,7 +67,9 @@ type Msg =
     | Location String
 --    | StartDate Date
 --    | EndDate Date
-    | Submit
+    | SubmitForm
+    | PostEventResponse (Result Http.Error String)
+    | Error String
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -58,8 +84,15 @@ update msg model =
     --        {model | end = endDate}
     --    StartDate startDate ->
     --        {model | start = startDate}
-        Submit ->
-            (model, Cmd.none)
+        SubmitForm ->
+            (model, postEventSend model)
+        PostEventResponse result ->
+            let
+                maybeError = extractErrorFromResult result
+            in
+            ({model | error = maybeError}, Cmd.none)
+        Error error ->
+            ({ model | error = error}, Cmd.none)
 
 
 -- VIEW
@@ -72,7 +105,9 @@ view model =
     , input [ type_ "text", placeholder "Location", onInput Location ] []
 --    , input [ type_ "date", onClick StartDate ] []
 --    , input [ type_ "date", onClick EndDate ] []
-    , button [ ] [ text "Send" ]
+    , button [ onClick SubmitForm ] [ text "Send" ]
+    , br [] []
+    , viewError model
     ]
 
 
@@ -85,5 +120,46 @@ subscriptions model =
 
 -- HTTP
 
-submitEventCreation =
-    Http.post
+postEventCreation : Model -> Http.Request String
+postEventCreation model =
+    let
+        body =
+            model
+                |> formEncoder
+                |> Http.jsonBody
+    in
+        Http.post apiEventCreate body postEventResponseDecoder
+
+postEventSend : Model -> Cmd Msg
+postEventSend model =
+    Http.send PostEventResponse (postEventCreation model)
+
+-- LOGGING
+
+-- ERRORS
+viewError : Model -> Html msg
+viewError model =
+  let
+    (color, message) =
+      if model.error == "" then
+        ("green", "No errors")
+      else
+        ("red", model.error)
+  in
+    div [ style [("color", color)] ] [ text message ]
+
+errorToString : Http.Error -> String
+errorToString error =
+    case error of 
+        Http.BadUrl something -> "Bad Url: " ++ something
+        Http.Timeout -> "Timeout"
+        Http.NetworkError -> "Network Error"
+        Http.BadStatus _ -> "Bad Status"
+        Http.BadPayload _ _-> "Dunno"
+
+extractErrorFromResult : Result Http.Error String -> String
+extractErrorFromResult result =
+    case result of
+        Ok _ -> ""
+        Err error ->
+            errorToString error
