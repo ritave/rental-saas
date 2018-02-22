50b7f357bf7b99575afc5c33ee19be628c59b2db
cymerrad <cymerrad+git@gmail.com>
2018-02-22 13:22:40 +0100

    I think I'm going to try switching to the newest version of all these packages

--- front/my_try2.elm
+++ front/my_try2.elm
@@ -1,37 +1,89 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Http
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+eventServer : String
+eventServer =
+    ""
 
 -- MODEL
 
 type alias Model =
-  { ...
-  }
+    { summary : String
+    , user : String
+    , location : String
+--    , start : Date
+--    , end : Date
+    }
 
+model : Model
+model =
+  Model "" "" ""
+
+-- INIT
+
+init : (Model, Cmd Msg)
+init =
+    (model, Cmd.none)
 
 -- UPDATE
 
-type Msg = Submit | ...
+type Msg =
+    Summary String
+    | User String
+    | Location String
+--    | StartDate Date
+--    | EndDate Date
+    | Submit
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  ...
+    case msg of
+        Summary summary ->
+            ({model | summary = summary}, Cmd.none)
+        User user ->
+            ({model | user = user}, Cmd.none)
+        Location location ->
+            ({model | location = location}, Cmd.none)
+    --    EndDate endDate ->
+    --        {model | end = endDate}
+    --    StartDate startDate ->
+    --        {model | start = startDate}
+        Submit ->
+            (model, Cmd.none)
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  ...
+  div []
+    [ input [ type_ "text", placeholder "Email", onInput User ] []
+    , input [ type_ "text", placeholder "Summary", onInput Summary ] []
+    , input [ type_ "text", placeholder "Location", onInput Location ] []
+--    , input [ type_ "date", onClick StartDate ] []
+--    , input [ type_ "date", onClick EndDate ] []
+    , button [ ] [ text "Send" ]
+    ]
 
 
+-- TODO
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  ...
+    Sub.none
 
+-- HTTP
 
--- INIT
-
-init : (Model, Cmd Msg)
-init =
-  ...
+submitEventCreation =
+    Http.post
