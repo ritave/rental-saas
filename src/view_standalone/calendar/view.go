166c9771fabdd9862c66f746da32bb093d713535
Radosław Cymer <cymerrad@gmail.com>
2018-03-21 23:25:30 +0100

    View handler | if I ever finish this, I will succeed at making almost-Java from Golang :|

--- src/view_standalone/calendar/view.go
+++ src/view_standalone/calendar/view.go
@@ -1,43 +1,15 @@
 package calendar
 
 import (
-	"rental-saas/src/calendar_wrap"
 	"rental-saas/src/model"
-	"encoding/json"
-	"net/http"
-	"google.golang.org/appengine"
-	"log"
-	gaeLog "google.golang.org/appengine/log"
+	"rental-saas/src/presenter/wrapper"
 )
 
-func View(w http.ResponseWriter, r *http.Request) {
-	srv := calendar_wrap.NewStandard(r)
-	ctx := appengine.NewContext(r)
+type ViewRequest struct{}
+type ViewResponse []*model.Event
 
-	events, err := srv.Events.List("primary").ShowDeleted(false).OrderBy("updated").Do()
-	if err != nil {
-		gaeLog.Debugf(ctx, "Listing events %s", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	result := make([]model.Event, 0)
-	for _, ev := range events.Items {
-		converted := model.ConvertGoogleToMine(ev)
-		result = append(result, *converted)
-	}
-	bytez, err := json.Marshal(&result)
-	if err != nil {
-		gaeLog.Debugf(ctx, "Marshalling events %s", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	if appengine.IsDevAppServer() {
-		log.Println("We will be sending this back:")
-		log.Println(string(bytez))
-	}
-
-	w.Write(bytez)
+func View(a *wrapper.Application, r interface{}) (interface{}, error) {
+	//events, err := srv.Events.List("primary").ShowDeleted(false).OrderBy("updated").Do()
+	return a.Calendar.QueryEvents()
 }
 
